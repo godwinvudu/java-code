3c4757ab46d3196804b3cc91f49e22836badfe27
Godwin Vudu <adoniscrowe@gmail.com>
2026-07-25 15:22:33 +0000

    tired

---
+++ easy.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class easy{
+      public static void main(String[] args) {
+        try (Scanner scanner= new Scanner(System.in)){
+             System.out.println("what's your name? ");
+             String name = scanner.nextLine(); 
+             System.out.println("hello "+ name);
+             System.out.println("how old are you?");
+             int age = scanner.nextInt();
+             System.out.println("so you're "+age);
+             
+             
+    }
+    }
+}      
+       
+        
+
