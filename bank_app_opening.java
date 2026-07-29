22f7f6b8f2382de34bf8ec1ff5a1c1f46f96d08c
Godwin Vudu <adoniscrowe@gmail.com>
2026-07-29 16:41:38 +0000

    Refactor input validation for first name, surname, and email in bank_app_opening.java

--- bank_app_opening.java
+++ bank_app_opening.java
@@ -1,74 +1,51 @@
 import java.util.Scanner;
 //this is a java program replicating a bank form
 public class bank_app_opening {
+
+}
     public static void main( String[]args){
         try(Scanner scanner=new Scanner(System.in)){
         System.out.println("#######-Welcome to adonis bank ltd-#######");
       
         System.out.println("Enter your first name?");
-        String first_name =scanner.nextLine();
-        while(first_name.isEmpty()){
+        String first_name = scanner.nextLine();
+
+        while (first_name.trim().isEmpty()){
             System.out.println("invalid input");
-        
-        System.out.println("Enter your first name?")
-        ;
-         String new_first_name =scanner.nextLine();
+            System.out.println("Enter your first name?");
+            first_name =scanner.nextLine();
         
         }
         
         System.out.println("Enter your surname:");
         String second_name =scanner.nextLine();
         
-        while(second_name.isEmpty()){
+        while(second_name.trim().isEmpty()){
             System.out.println("invalid second name");
-
-        System.out.println("Enter your surname:");
-
-            String new_second_name =scanner.nextLine();
+            System.out.println("Enter your surname:");
+            second_name =scanner.nextLine();
         
-        break;
+    
         }
         
-        System.out.println("Thank you "+ (first_name)+" "+ second_name + " for trusting in adaonis bank ltd");
+        System.out.println("Thank you "+ (first_name)+" "+ (second_name) + " for trusting in adaonis bank ltd");
        
         System.out.println("Enter your email:");
-         String email=scanner.nextLine();
-        while(true){
-        if (email.contains("@") && email.contains(".") ){
-            System.out.println("email is valid"); 
-
-            if (email.endsWith("@gmail.com")) {
-            System.out.println("gmail account is valid");
-        }  
-                else if (email.endsWith("@yahoo.com")){
-                    System.out.println("yahoo account is valid");
-                }
-                    else{
-                        System.out.println("email is invalid");
-                    }
-        }
-    
-    
-
-        System.out.println("create password:");
-         String password=scanner.nextLine();
-         String specailcharacters="!@#$%^&*()_+=-:'?><,./~`'";
-        if ((password.length() >=10) || (password.contains(specailcharacters))){
-            System.out.println("re-enter password");
-                String re_password=scanner.nextLine();
-            if(re_password.equals(password)){
-                System.out.println("password accepted");
-
-            } 
-                else
-                    System.out.println("password does not match");
+        String email=scanner.nextLine();
+        
+        while(email.trim().isEmpty()){
+            System.out.println("invalid email");
+            System.out.println("Enter email: ");
+            email=scanner.nextLine();
 
         }
+        while(email.contains("@") ||!email.contains(".")){
+            System.out.println("email is invalid.must countain");
+            System.out.println("enter a valid email: ");
+            email=scanner.nextLine();
         }
-
-
-
-       }
+        
+        }
     }
     
-}
+    
