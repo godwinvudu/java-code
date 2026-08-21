1e570ebd0c83657f4b40bc16e8586be6268c3de0
Godwin Vudu <adoniscrowe@gmail.com>
2026-08-21 12:39:39 +0000

    Add whitespace for improved readability in passquestion class

--- .vscode/passquestion.java
+++ .vscode/passquestion.java
@@ -2,5 +2,8 @@ public class passquestion {
     //let's talk about object oriented design ;when given a scenario, the nouns in that scenario are the CLASSES,the task or activities(verbs) they perform are the METHODS and the adjectives are ATRIBUTES
 //THE COMMENT ABOVE POINTS TO THE TEXTUAL ANALYSIS(Abbott's technique)
 
+
+
+
 }
 
