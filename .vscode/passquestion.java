702ddceef5e695898043b18867cb42ab0259c715
Godwin Vudu <adoniscrowe@gmail.com>
2026-08-24 23:04:32 +0000

    Fix typo in output message in passquestion class

--- .vscode/passquestion.java
+++ .vscode/passquestion.java
@@ -2,6 +2,7 @@ public class passquestion {
     //let's talk about object oriented design ;when given a scenario, the nouns in that scenario are the CLASSES,the task or activities(verbs) they perform are the METHODS and the adjectives are ATRIBUTES
 //THE COMMENT ABOVE POINTS TO THE TEXTUAL ANALYSIS(Abbott's technique)
 public static void main(String[]args){
-    System.out.println(".(GOOD MORNIG)");
+    System.out.println("(GOOD MORNIG)");
+}
 }
 
